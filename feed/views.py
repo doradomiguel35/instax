94ab018aa891711dbde7c3c8b72930d3c777c564
Miguel Dorado <doradomiguel35@gmail.com>
2018-12-13 01:32:48 +0800

    see people who liked the posts

--- feed/views.py
+++ feed/views.py
@@ -7,11 +7,16 @@ from .forms import CommentForm,LikeForm
 from django.http import JsonResponse
 from .serializers import CommentSerialize,FeedSerialize,LikesSerialize
 from django.shortcuts import render
+from django.core import serializers
+from django.contrib.auth.models import User
+import json
 
 
 class FeedsView(TemplateView):
 	"""
 	Feeds View
+	GET - render template, send context data
+	POST - return json response to display recently added comments
 
 	"""
 	template_name = 'feed/users_page.html'
@@ -25,7 +30,11 @@ class FeedsView(TemplateView):
 		self.feed_data._result_cache = None
 
 		return render(self.request, self.template_name,{
-			'feed_data': self.feed_data,'comment_data':self.comment_data,'user_data':self.request.user,'comment_form':self.comment_form,'like_field':self.like_field})
+			'feed_data': self.feed_data,
+			'comment_data':self.comment_data,
+			'user_data':self.request.user,
+			'comment_form':self.comment_form,
+		})
 
 	def post(self,request, *args, **kwargs):
 		form = CommentForm(self.request.POST)	
@@ -45,13 +54,20 @@ class FeedsView(TemplateView):
 
 class LikeView(View):
 	"""
-	Likes View
+	Likes View 
+	GET - retrieve username and prof pic, return json for displaying accounts that liked the post
+	POST - each user can like a post only once, if a post is already liked, then the user clicked the like button, it will be unliked
 
 	"""
-	
+	def get(self,request,*args,**kwargs):
+		likers = LikesUser.objects.filter(feed_id=kwargs.get('feed_id'),liked=True).values('user__username','user__account__prof_pic')
+		serializer = {'data': list(likers)}
+		return JsonResponse(serializer, safe=False)
+		
+
 	def post(self,request,*args,**kwargs):
 		feed_data = Feeds.objects.get(id=kwargs.get('feed_id'))
-		# import pdb; pdb.set_trace()
+		
 		try:
 			like_user = LikesUser.objects.get(feed_id=kwargs.get('feed_id'),user_id=self.request.user.id)
 			print('try')
@@ -62,8 +78,8 @@ class LikeView(View):
 				feed_data.save()
 				like_user.save()
 				feed_serializer = FeedSerialize(feed_data)
-				json = feed_serializer.data
-				return JsonResponse(json, safe=False)
+				serialized = feed_serializer.data
+				return JsonResponse(serialized, safe=False)
 				
 			else:
 				print('else')
@@ -72,17 +88,18 @@ class LikeView(View):
 				feed_data.save()
 				like_user.save()
 				feed_serializer = FeedSerialize(feed_data)
-				json = feed_serializer.data
-				return JsonResponse(json, safe=False)
+				serialized = feed_serializer.data
+				return JsonResponse(serialized, safe=False)
 				
 		except:
 			print('exception')
 			like_user = LikesUser(feed_id=kwargs.get('feed_id'),user_id=self.request.user.id,liked=True)
-			like_user.save()
 			feed_data.likes+=1
+			like_user.save()
 			feed_data.save()
 			feed_serializer = FeedSerialize(feed_data)
 			json = feed_serializer.data
 			return JsonResponse(json, safe=False)
+
 
 			
